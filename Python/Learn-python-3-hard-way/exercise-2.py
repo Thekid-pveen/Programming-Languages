bb0d52405bae70641fd4c8c32dc92461dc26614a
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-10-24 22:32:17 +0530

    Create exercise-2.py

---
+++ Python/Learn-python-3-hard-way/exercise-2.py
@@ -0,0 +1,10 @@
+# title: Comments and Pound Characters
+# Comments are very important in your programs
+
+"""
+Comments to explain later what is the code to do
+
+it’s called (octothorpe or pound character)
+
+Easy to fix bugs on your program
+"""
