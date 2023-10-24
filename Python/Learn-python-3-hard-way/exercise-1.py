99b3ec25e86f92a4e0d524c94aaa279017bbc8cd
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-10-24 22:11:09 +0530

    Create exercise-1.py

---
+++ Python/Learn-python-3-hard-way/exercise-1.py
@@ -0,0 +1,8 @@
+# print function to Display Anything
+
+print("Hello Word!")
+print("Again and Again")
+print("I like this")
+print("Just I'm ordinary person")
+print("I 'hate' the life")
+print('I am a "overthinker"')
