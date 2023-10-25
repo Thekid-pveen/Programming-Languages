02ba90b5fa540127cff511e7428f21b9df93632b
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-10-25 22:57:29 +0530

    Create exercise-3.py

---
+++ Python/Learn-python-3-hard-way/exercise-3.py
@@ -0,0 +1,25 @@
+# title: Numbers and Math
+# numbers and math are very interesting and fun
+
+print("-Addition-")
+print(99 + 1)
+
+print("-Subraction-")
+print(101 - 1)
+
+print("-Multiplication-")
+print(10 * 10)
+print(10 ** 2)
+
+print("-Divison-")
+print(1000//10)
+print(1000/10)
+
+print("-Grater-than & Less-than-")
+print(200//2 == 1000/10)
+print(100 < 10)
+print(100 > 1)
+print(11 <= 1)
+print(0 <= 0)
+#--------------------------------
+
