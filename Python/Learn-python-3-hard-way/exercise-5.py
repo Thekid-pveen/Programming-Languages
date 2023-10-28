4da042dbe5ddb09fc6838e3a1f2e6d5c978f350c
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-10-28 11:55:47 +0530

    Create exercise-5.py

---
+++ Python/Learn-python-3-hard-way/exercise-5.py
@@ -0,0 +1,12 @@
+# title: More Variables and Printing
+# Love is always a leap into the unknown
+
+my_name = 'Pravee'
+fav_color = 'white'
+girl_frd = 'Rekshi'
+my_dog = 'apoo'
+
+print(f"Let's talk about {my_name}.")
+print(f'my favourite color is {fav_color}.')
+print(f'I like my girl friend {girl_frd}.')
+print(f"my dog name is {my_dog}.")
