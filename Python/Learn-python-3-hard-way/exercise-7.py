7d59d8d65a3a5ab7f1c1f5c559ee449456d999d8
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-10-28 12:42:40 +0530

    Create exercise-7.py

---
+++ Python/Learn-python-3-hard-way/exercise-7.py
@@ -0,0 +1,24 @@
+# title: More Printing
+# Print is Sprint
+
+print("I had a little lamb.")
+print("It's fleece was white as {}.".format('snow'))
+print("And everywhere that I went.")
+print("." * 11)  # In this code mulitiply the string
+
+end1 = "M"
+end2 = "i"
+end3 = "l"
+end4 = "k"
+end5 = "M"
+end6 = "u"
+end7 = "s"
+end8 = "i"
+end9 = "c"
+
+# note the end
+print(end1 + end2 + end3 + end4, end=' ')
+print(end5 + end6 + end7 + end8 + end9)
+
+
+
