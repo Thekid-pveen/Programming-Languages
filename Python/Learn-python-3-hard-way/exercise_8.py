9d9da7245b256a473a98f1fc9bd1c5b181ebe113
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-10-29 20:23:46 +0530

    Create exercise_8.py

---
+++ Python/Learn-python-3-hard-way/exercise_8.py
@@ -0,0 +1,14 @@
+# title: Printing, Printing
+# Printing is a good visualization
+
+formatter_str = " {} {} {} "
+
+lycris_1 = formatter_str.format("Okay,", "I realize now,", "that everything that I did was wrong")
+lycris_2 = formatter_str.format("Okay,", "I realize now,", "some things are better off said than done")
+lycris_3 = formatter_str.format("Okay,", "I realize now,", "that maybe I'm not ready for love")
+lycris_4 = formatter_str.format("Okay,", "I realize now,", "I finished us before we begun-un-un")
+
+print(lycris_1)
+print(lycris_2)
+print(lycris_3)
+print(lycris_4)
