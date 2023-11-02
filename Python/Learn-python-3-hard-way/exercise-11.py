ad5b87ce85ba6a0b7a2692f61bd71d43b396951e
praveen <128697412+praveen-pravee@users.noreply.github.com>
2023-11-02 23:53:01 +0530

    Create exercise-11.py

---
+++ Python/Learn-python-3-hard-way/exercise-11.py
@@ -0,0 +1,13 @@
+# title: Ask Questions
+# fun input()
+
+print("Who Are You _",end=' ')
+who = input()
+
+print("What Are You Doing _",end=' ')
+do = input()
+
+print("What's Going _",end=' ')
+go = input()
+
+print(f"Good Name {who}, {do} Doing well, keep it up {go}.")
