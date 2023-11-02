56ef472e8fb885cf9aa04f021072c83588ef02f6
praveen <128697412+praveen-pravee@users.noreply.github.com>
2023-11-02 23:48:04 +0530

    Create exercise-9.py

---
+++ Python/Learn-python-3-hard-way/exercise-9.py
@@ -0,0 +1,21 @@
+# title: Printing, Printing, Printing
+# Just Following and Keep Going The Book
+
+days = "Sun Mon Tue Wed Thu Fri Sat"
+months = " Jan\n Feb\n Mar\n Apr\n May\n Jun\n Jul\n Aug\n Set\n Nov\n Dec"
+
+print("Here are The days")
+print(days)
+
+print("Here are The months in a year")
+print(months)
+
+# double-quote printing 
+
+print("""
+Hello there, What'sApp? 
+What's going on Dude?   
+Are you ok with that?   
+keep going, and thing is
+Trust The Process Dude. 
+""")
