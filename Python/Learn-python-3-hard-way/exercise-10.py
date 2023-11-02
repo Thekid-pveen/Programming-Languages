ab1d27a59e0c7ef706410c23e3c6141b466ff27e
praveen <128697412+praveen-pravee@users.noreply.github.com>
2023-11-02 23:49:30 +0530

    Create exercise-10.py

---
+++ Python/Learn-python-3-hard-way/exercise-10.py
@@ -0,0 +1,18 @@
+# title: What Was That?
+# Using escape-character into your str
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm spilt\non a line."
+backslash_cat = "I'm \\ in \\ a \\ car."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Milk and\n \t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
