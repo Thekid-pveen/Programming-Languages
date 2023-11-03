e7f9d5d473767fc6683e72c59f43cd01213c1bfc
praveen <128697412+praveen-pravee@users.noreply.github.com>
2023-11-03 12:53:17 +0530

    Update exercise-13.py

--- Python/Learn-python-3-hard-way/exercise-13.py
+++ Python/Learn-python-3-hard-way/exercise-13.py
@@ -1,1 +1,11 @@
+# title: Parameters, Unpacking, Variables
+# Hold Up! Features Have Another Name
 
+from sys import argv # run the file with argv
+
+script, first, second = argv
+
+print("The Script called is",script)
+print("first name is ",first)
+print("second name is ",second)
+  
