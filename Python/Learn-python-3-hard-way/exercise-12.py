f74ab2268d3780234b1de724473f0310ce5a4ca8
praveen <128697412+praveen-pravee@users.noreply.github.com>
2023-11-03 12:36:46 +0530

    Update exercise-12.py

--- Python/Learn-python-3-hard-way/exercise-12.py
+++ Python/Learn-python-3-hard-way/exercise-12.py
@@ -1,1 +1,14 @@
+# title: Prompting People
+# Put Into Variable In Input
 
+py = input("What Programming Language Do you Learn ?")
+ecs = input("Eat Code SLeep ?")
+ai = input("AI stands for ?")
+
+print("\ndifference between Last ex and this program ?\n",end=' ')
+diff=input("input() takes no keyword arguments")
+
+print(diff)
+
+print(f"""ok. Now you learn {py} and your mindset is {ecs}.
+Do you know AI: {ai}.""") 
