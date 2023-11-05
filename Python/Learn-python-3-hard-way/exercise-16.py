2b81adf086a73bfed1460376d36d16bd89029666
praveen <128697412+praveen-pravee@users.noreply.github.com>
2023-11-05 23:47:11 +0530

    Not complete....!

---
+++ Python/Learn-python-3-hard-way/exercise-16.py
@@ -0,0 +1,12 @@
+# title: Reading and Writing Files
+# I don't know what i can do in the exercise! 
+
+from sys import argv
+
+script, filename = argv
+
+print(f"We are going to earse {filename}.")
+print("If you don't want that, hit CTRL-C (C^).")
+print("If you do want that, hit RETURN.")
+
+
