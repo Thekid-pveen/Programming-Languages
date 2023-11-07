3bf4a9f34627d45d14ee021d178241e7292d1f75
praveen <128697412+praveen-pravee@users.noreply.github.com>
2023-11-07 18:08:08 +0530

    Update exercise-16.py

--- Python/Learn-python-3-hard-way/exercise-16.py
+++ Python/Learn-python-3-hard-way/exercise-16.py
@@ -1,12 +1,37 @@
 # title: Reading and Writing Files
-# I don't know what i can do in the exercise! 
-
-from sys import argv
+# now we are working with files
+from sys import argv 
 
 script, filename = argv
 
-print(f"We are going to earse {filename}.")
-print("If you don't want that, hit CTRL-C (C^).")
+print(f"We Are going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
 print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening File...")
+target = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line_1 = input("line 1")
+line_2 = input("line 2")
+line_3 = input("line 3")
+
+print("I'm going to write these to the file.")
+
+target.write(line_1)
+target.write("\n")
+target.write(line_2)
+target.write("\n")
+target.write(line_3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
 
 
