79c8fd999d73191689e140e8078d030af7c80d90
praveen <128697412+praveen-pravee@users.noreply.github.com>
2023-11-24 23:45:26 +0530

    Create exercise-22.py

---
+++ Python/Learn-python-3-hard-way/exercise-22.py
@@ -0,0 +1,8 @@
+# What Do You Know So Far?
+# D0N'T forget something
+
+"""
+I decided to upgrade all the Python exercise programs.
+time to review all the things, what I will learn so 
+There won't be any code in this exercise.
+"""
