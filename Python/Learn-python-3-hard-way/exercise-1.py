ceafdcc207a486cd748097a0c632f4e98892a446
praveen <128697412+praveen-pravee@users.noreply.github.com>
2023-11-25 00:04:56 +0530

    A GOOD FIRST PROGRAM 1

--- Python/Learn-python-3-hard-way/exercise-1.py
+++ Python/Learn-python-3-hard-way/exercise-1.py
@@ -1,9 +1,17 @@
 # title: A Good First Program
 # print function to Display Anything
 
+"""
+Study Drills:
+This is a first exercise study drill, that explains or things you should try do to. Make it clear to understand what I do.
+Other programs may contain lots of information about what I doing at the exercise. learning to make mistakes, perfectly it's
+good for anyone. I trust the process. 
+
+"""
 print("Hello Word!")
 print("Again and Again")
 print("I like this")
 print("Just I'm ordinary person")
 print("I 'hate' the life")
 print('I am a "overthinker"')
+
