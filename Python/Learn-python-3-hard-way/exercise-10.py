7e9a4163e1500876f09688eacd88e41ed98fbbcd
praveen-pravee <praveenja0010@gmail.com>
2024-07-06 14:57:48 +0530

    Updating SD.

--- Python/Learn-python-3-hard-way/exercise-10.py
+++ Python/Learn-python-3-hard-way/exercise-10.py
@@ -1,6 +1,13 @@
 # title: What Was That?
 # Using escape-character into your str
-
+"""
+STUDY DRILLS
+------------
+I showed you two ways to make a string that goes across multiple lines.
+Already we learned '\n' this is new line code, And new character \n \t \\ 
+and There are various escape sequences available for different characters 
+you might want to use.
+"""
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm spilt\non a line."
 backslash_cat = "I'm \\ in \\ a \\ car."
