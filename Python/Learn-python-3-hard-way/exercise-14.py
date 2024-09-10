0cbd0ba69126197dffa52fbd1c0a5a580646a321
praveen <128697412+praveen-pravee@users.noreply.github.com>
2024-09-10 14:52:57 +0530

    Update exercise-14.py

--- Python/Learn-python-3-hard-way/exercise-14.py
+++ Python/Learn-python-3-hard-way/exercise-14.py
@@ -1,6 +1,13 @@
 # title: Prompting and Passing
 # More Input you can make
-
+"""
+STUDY DRILLS:
+-------------
+Let’s do one exercise that uses argv and input together to ask the user
+something specific. You will need this for the next exercise where you learn to
+read and write files. In this exercise we’ll use input slightly differently by
+having it print a simple > prompt.
+"""
 from sys import argv
 
 script, name, passwd = argv
