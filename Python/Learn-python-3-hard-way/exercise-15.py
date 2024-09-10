2aef223587f3da82c2da8b5f0998a7648052278e
praveen <128697412+praveen-pravee@users.noreply.github.com>
2024-09-10 15:30:30 +0530

    Update exercise-15.py

--- Python/Learn-python-3-hard-way/exercise-15.py
+++ Python/Learn-python-3-hard-way/exercise-15.py
@@ -1,6 +1,12 @@
 # title: Reading Files  
 # Working with files
-
+"""
+STUDY DRILLS:
+-------------
+This exercise involves writing two files. One is the usual ex15.py file that you
+will run, but the other is named sample.txt this one you want to created. This second file isn’t a
+script but a plain text file we’ll be reading in our script. the new commands are open() & read() stay focus.
+"""
 from sys import argv
 
 script, filename = argv
