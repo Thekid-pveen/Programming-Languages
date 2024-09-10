ffb24f1c5d4918e02622cbdb075e4a664e10ade1
praveen <128697412+praveen-pravee@users.noreply.github.com>
2024-09-10 14:21:41 +0530

    Update exercise-12.py

--- Python/Learn-python-3-hard-way/exercise-12.py
+++ Python/Learn-python-3-hard-way/exercise-12.py
@@ -1,6 +1,13 @@
 # title: Prompting People
 # Put Into Variable In Input
+"""
+STUDY DRILLS:
+-------------
+This is how you ask someone a question and get the answer, Put a string that you want
+for the prompt inside the () so that it looks like this: x = input("Name? "), 
+these the exercise is also like the previous one using just input to do all the prompting.
 
+"""
 py = input("What Programming Language Do you Learn ?")
 ecs = input("Eat Code SLeep ?")
 ai = input("AI stands for ?")
