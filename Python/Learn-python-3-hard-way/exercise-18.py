403d72fd7aafe0df213b2038cf08776005b1555f
praveen <128697412+praveen-pravee@users.noreply.github.com>
2024-10-03 18:53:22 +0530

    Update exercise-18.py

--- Python/Learn-python-3-hard-way/exercise-18.py
+++ Python/Learn-python-3-hard-way/exercise-18.py
@@ -1,8 +1,15 @@
 # title: Names, Variables, Code, Functions
 # Fuctions with Arguments
-
+"""
+STUDY DRILLS:
+-------------
+You can create a function by using the word def in Python. I’m going to have
+make four different functions, the functions are important to know. 
+I will give you the simplest explanation you can use right now.
+NOTE: Look out for the *args, which is different * to assign the number of values you can pass.
+"""
 def function_one(*args):    # *args is pointless
-    arg1, arg2 = args
+    arg1, arg2 = args      
     print(f"arg1 is {arg1} and arg2 is {arg2}.")
 
 def function_two(arg1, arg2): 
