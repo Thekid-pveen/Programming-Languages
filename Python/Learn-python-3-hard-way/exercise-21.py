ce6f956d3cad599e9d4b7bce7139dfe0107c8e55
praveen <128697412+praveen-pravee@users.noreply.github.com>
2024-10-04 12:46:41 +0530

    Update exercise-21.py

--- Python/Learn-python-3-hard-way/exercise-21.py
+++ Python/Learn-python-3-hard-way/exercise-21.py
@@ -1,6 +1,12 @@
 # Functions Can Return Something
 # Function 
-
+"""
+STUDY DRILLS:
+-------------
+We are now doing our math functions for adding, subtracting, multiplying, and dividing.
+Return to calculate the value just enough. This exercise might whack your brain out,
+but treat it slowly and easily like a little game.
+"""
 def add(a, b):
 	print(f"ADDING {a} + {b}")
 	return a + b
@@ -29,3 +35,4 @@ print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
 print("Here is a puzzle")
 
 what = add(age, subtract(height, multiply(weight, divide(iq,2))))
+print("That is a collapse in " + what)
